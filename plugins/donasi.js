60730b773ae3dc229c789ea01dc4eb8e99c8f947
Dyandra123 <90011515+Dyandra123@users.noreply.github.com>
2021-09-03 09:44:24 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,10 +1,11 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => conn.sendButtonLoc(m.chat, await (await fetch(fla + 'donasi')).buffer(), `
 ┌〔 Donasi • Emoney 〕
-├ https://saweria.co/ariffb
-├ https://trakteer.id/ariffb/tip
+├ https://saweria.co/DyanLiaBOT
+├〔 Sport temenya ya 〕
+├ https://trakteer.id/trito-apriansyah/tip
 └────
-`.trim(), '© stikerin', 'Donasi', '.donasi')
+`.trim(), '© auliabot', 'Donasi', '.donasi')
 handler.help = ['donasi']
 handler.tags = ['info']
 handler.command = /^dona(te|si)$/i
